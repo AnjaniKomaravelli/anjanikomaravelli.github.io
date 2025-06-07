dee6936406f3404477dec78befc7bca7df4fd537
anjanikomaravelli <anjanikomaravelli@gmail.com>
2025-06-07 11:49:46 +0530

    changed

--- p2/script.js
+++ p2/script.js
@@ -1,23 +1,36 @@
+const users = [];
+let user={}
 const showLogin = () => {
     let str=`
     <div class='App-Container'>
     <h1>Login Form</h1>
     <p><input type="text" id="txtEmail"></p>
     <p><input type="password" id="txtPass"></p>
-    <p><button>Log In</button></p>
+    <p><button onclick='welcome()'>Log In</button></p>
     <p><button onclick='showRegister()'>Create Account</button></p>
     </div>
     `
     root.innerHTML = str
 }
-
 const showRegister = () => {
       let str=`
     <h1>Register Form</h1>
     <p><input type="text" id="txtName"></p>
      <p><input type="text" id="txtEmail"></p>
     <p><input type="password" id="txtPass"></p>
-    <button>Register</button>
+    <button onclick='adduser()'>Register</button>
+    <button onclick='showlogin()'>Register<button</button>
     `
     root.innerHTML = str
 }
+
+const adduser= () => {
+    const user= {
+    name: getElementByID("txtName").value
+    email: getElementByID("txtEmail").value
+    pass: getElementByID("txtPass").value
+    };
+    users.push(user);
+    console.log(users);
+    showlogin();
+};
